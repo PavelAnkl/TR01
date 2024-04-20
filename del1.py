5d60de62671b4863cd4eaa4c603420307ec72275
PavelAnkl <kluev-pa@yandex.ru>
2024-04-20 06:03:01 +0300

    all lesson

---
+++ del1.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conn = sqlite3.connect('example.db')
+cur = conn.cursor()
+
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS users
+    (id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL)
+        ''')
+
+conn.commit()
+
+cur.execute("INSERT INTO users (name, age) VALUES (?, ?)",("Алексей", 30))
+cur.execute("INSERT INTO users (name, age) VALUES (?, ?)",("Игорь", 25))
+
+conn.commit()
+
+cur.execute("SELECT * FROM users")
+rows = cur.fetchall()
+print(rows)
+
+conn.close()
